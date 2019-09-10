8ff26792fe72b4325a01e701029fa4406484d0fd
Dasmish <kosty.filipenko@gmail.com>
2019-09-10 11:10:43 +0300

    Cont

--- src/Components/MainGame.jsx
+++ src/Components/MainGame.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
+import Player from './Player'
 
 //Элементы игры
-import drawPlayer from './Player'
 
 export default class GameField extends React.PureComponent {
-
-    constructor(props) {
-        super(props);
-    }
 
     resizeCanvas = () => {
         var canId = document.querySelectorAll('#canvas')[0];
         canId.width = window.innerWidth;
         canId.height = window.innerHeight;
     }
-
-    
-
     
 
     componentDidMount() {
--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -6,9 +6,15 @@ export default class Player extends React.PureComponent {
         super(props);
     }
 
+
     //Отрисовка Игрока
     drawPlayer = () => {
         const cont = this.refs.canvas.getContext('2d');
         cont.strokeRect(180, window.innerHeight - 400, 101, 257);
     }
+
+    test_1 = function test(params) {
+        return test;
+    }
+    
 }
